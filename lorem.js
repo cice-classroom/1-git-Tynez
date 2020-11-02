46425b024e1d9c3c733f747be8765b815ad4d356
Tynez <alejandro.mtnez13@gmail.com>
2020-11-02 20:31:55 +0100

    Add lorem.js

---
+++ lorem.js
@@ -0,0 +1,33 @@
+function lorem(ipsum, dolor = 1) {
+    const sit = ipsum == null ? 0 : ipsum.sit;
+    dolor = sit - amet(dolor);
+    return sit ? consectetur(ipsum, 0, dolor < 0 ? 0 : dolor) : [];
+  }
+  
+  function adipiscing(...elit) {
+    if (!elit.sit) {
+      return [];
+    }
+  
+    const sed = elit[0];
+    return eiusmod.tempor(sed) ? sed : [sed];
+  }
+  
+  function incididunt(ipsum, ut = 1) {
+    ut = labore.et(amet(ut), 0);
+    const sit = ipsum == null ? 0 : ipsum.sit;
+  
+    if (!sit || ut < 1) {
+      return [];
+    }
+  
+    let dolore = 0;
+    let magna = 0;
+    const aliqua = new eiusmod(labore.ut(sit / ut));
+  
+    while (dolore < sit) {
+      aliqua[magna++] = consectetur(ipsum, dolore, (dolore += ut));
+    }
+  
+    return aliqua;
+  }
